634119bf56aeab0c3a21ebd445e23a445249dbe6
Grincheek <warning686@gmail.com>
2023-06-07 11:33:45 +0300

    Classwork if-else

--- Second_Lesson/Second_Leson.py
+++ Second_Lesson/Second_Leson.py
@@ -1,21 +1,29 @@
-import random
-
-number = int(input("Enter day number: "))
-# print(f"{number}")
-# number = random.randint(1, 7)
-if number == 1:
-    print("Monday")
-elif number == 2:
-    print("Tuseday")
-elif number == 3:
-    print("Wednesday")
-elif number == 4:
-    print("Thursday")
-elif number == 5:
-    print("Friday")
-elif number == 6:
-    print("Saturday")
-elif number == 7:
-    print("Sunday")
-else:
-    print("There are no days with this number.")
+# number = int(input("Enter day number: "))
+# # print(f"{number}")
+# # number = random.randint(1, 7)
+# if number == 1:
+#     print("Monday")
+# elif number == 2:
+#     print("Tuseday")
+# elif number == 3:
+#     print("Wednesday")
+# elif number == 4:
+#     print("Thursday")
+# elif number == 5:
+#     print("Friday")
+# elif number == 6:
+#     print("Saturday")
+# elif number == 7:
+#     print("Sunday")
+# else:
+#     print("There are no days with this number.")
+# a = int(input("Enter a "))
+# b = int(input("Enter b "))
+# print("Summ", a+b, "\nDiference", a-b, "\nProduction", a*b, "\nDivision", a/b)
+# if a > b:
+#     print("Max ", a, ", Min", b)
+# elif a < b:
+#     print("Max", b, ", Min", a)
+# else:
+#     print("a = b")
+# print("Average number ", (a+b)/2)
