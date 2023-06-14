74e937c1fe6491e62f6abce04f1352ecbb3b8af9
Grincheek <warning686@gmail.com>
2023-06-14 11:46:35 +0300

    Classwork

--- 4.Fourth_Lesson/Fourth_Lesson.py
+++ 4.Fourth_Lesson/Fourth_Lesson.py
@@ -1,15 +1,33 @@
 #===================LOOPS==============
-people = ["John","Adam","Sarah","Paul","Eva"]
+#people = ["John","Adam","Sarah","Paul","Eva"]
 '''for person in people:
     print(person)'''
-for person in people:
+'''for person in people:
     if person == "Sarah":
         print(person)
-        print(f"Index of wanted element: {people.index(person)}")
+        print(f"Index of wanted element: {people.index(person)}")'''
         
 '''for i in range(len(people)):
     print(people[i])'''
-'''count = 0
+'''count = 1
+#number = int(input("Enter number: "))
+number =1
+
 while count < 10:
-    print(f"Count: {count}")
-    count += 1 '''
+    print(number," * ",+ count," = ", +(number*count))
+    count += 1 
+number += 1
+'''
+'''exit = False
+while exit != True:
+    choise = int(input(f"Buy 36.57\tSale 37.45\n1. Buy\n2. Sell\n0. Exit\n"))
+    if choise ==1:
+        money = int(input(f"How many dolars do you want to buy? "))
+        print(f"You must pay {int (money * 37.45)} uah")
+    elif choise == 2:
+        money = int(input(f"How many uah do you have? "))
+        print(f"You earn {int (money / 36.57)} usd")
+    elif choise ==0:
+        exit=True'''
+
+    
