7b1f9ced20204cf988b0d7295e8c28aea7958958
Grinchik <warning686@gmail.com>
2023-06-21 22:28:01 +0300

    Fiveth homework by python basic cource

---
+++ 5.Fiveth_Homework/Fiveth_homework.py
@@ -0,0 +1,31 @@
+# ===============First task=============
+import random
+'''my_list = []
+for i in range(5):
+    my_list.append(random.randint(1, 10))
+print(my_list)
+for i in range(4):
+    for j in range(5-i-1):
+        if my_list[j] > my_list[j+1]:
+            my_list[j], my_list[j+1] = my_list[j+1], my_list[j]
+print(my_list)'''
+# ===============Second task=========================
+'''my_list = []
+my_set = {0}
+my_set.clear()
+for i in range(5):
+    my_list.append(random.randint(1, 10))
+print(f"List is: {my_list}")
+for i in range(5):
+    my_set.add(my_list[i])
+print(f"Set is: {my_set}")
+'''
+# ================Third task==================
+'''my_list = []
+for i in range(10):
+    my_list.append(random.randint(1, 10))
+print(f"Whole list: {my_list}")
+for i in range(10):
+    if (my_list[i]) % 3 == 0:
+        print(f"It is firsn odd number {my_list[i]}")
+        break'''
