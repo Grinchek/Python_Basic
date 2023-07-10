a329169b36b17188a4c96224aecff70bea343c94
DEV <dev@step.rivne>
2023-07-10 11:45:44 +0300

    Telegram bot2

--- 11.Bot/start.py
+++ 11.Bot/start.py
@@ -1,5 +1,5 @@
 import os
-
+from telebot import types
 import telebot
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
@@ -8,7 +8,10 @@ bot = telebot.TeleBot('6394570120:AAHwDpG6jMy0pUMmVr-C3oR2zKpBZQmhUWg')
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.row("Download movie","Download playlist")
+    keyboard.row("Share phone number")
+    bot.send_message(message.chat.id,"Hello  {0.first_name} {0.last_name}. I am {1.first_name}".format(message.from_user,bot.get_me()),reply_markup=keyboard)
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
